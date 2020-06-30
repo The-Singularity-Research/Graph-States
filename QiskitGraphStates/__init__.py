ec78f14c4df68398751d559188d283db6bf58331
Amelie Schreiber <thesingularity.research@gmail.com>
2020-06-30 15:27:20 -0700

    update-setup

--- QiskitGraphStates/__init__.py
+++ QiskitGraphStates/__init__.py
@@ -1,4 +1,4 @@
 from QiskitGraphStates.QiskitBipartiteGraphState import BipartiteGraphState
 from QiskitGraphStates.QiskitGraphState import GraphState
 
-__all__ = ['QiskitGraphState.py', 'QiskitBipartiteGraphState.py']
+__all__ = ['QiskitGraphState.py', 'QiskitBipartiteGraphState.py', 'CirqGraphState', 'CirqBipartiteGraphState']
